415744430d7f8d0b79d0571baba78d5e1656d7fc
Joris Baum <joris.baum@sap.com>
2026-02-02 17:54:52 +0100

    Dereference pointer to make an actual copy
    I noticed that as my changes depend on `RawQuery` not being empty, but
    actually they were.

--- src/pkg/ingress/bindings/filtered_binding_fetcher.go
+++ src/pkg/ingress/bindings/filtered_binding_fetcher.go
@@ -74,7 +74,7 @@ func (f *FilteredBindingFetcher) FetchBindings() ([]syslog.Binding, error) {
 			continue
 		}
 
-		anonymousUrl := u
+		anonymousUrl := *u
 		anonymousUrl.User = nil
 		anonymousUrl.RawQuery = ""
 
